bf645ba7e3e9276374238db88803d9d956574146
Sergey Baishev <bss.ser007@gmail.com>
2022-06-08 21:46:01 +0500

    Reconnection

--- SteamWorkshopDownloaderExtension/userscript.js
+++ SteamWorkshopDownloaderExtension/userscript.js
@@ -18,7 +18,7 @@ var myButtonPosition = realButton.offsetWidth + 20;
 var button = document.createElement('div');
 
 button.innerHTML = `
-<input type="button" style="pointer-events:none;" class="btn_medium  btn_border_2px" value="please wait... analizing..." id="steamdownload1">
+<input type="button" style="pointer-events:none;" class="btn_medium  btn_border_2px" value="connecting..." id="steamdownload1">
 <div id="result"></div>
 `
 
@@ -45,28 +45,45 @@ $(document).ready(function()
 					ItemName: $(".workshopItemTitle")[0].innerHTML,
 					ActionID: 0
 				}
-	var ws = new WebSocket("ws://localhost:1024/");
-		
-	ws.onopen = function (event) {
-		data.ActionID	= 1	
-		ws.send(JSON.stringify(data))
-	}
+				
+	function openWebSocket() {
+		var ws = new WebSocket("ws://localhost:1024/");
+			
+		ws.onopen = function (event) {
+			$("#steamdownload1").attr("style", "pointer-events:none;")
+			$("#steamdownload1").attr("value", "please wait... analizing......")
+			$("#steamdownload1").attr("class", "btn_medium  btn_border_2px")
+			data.ActionID	= 1	
+			ws.send(JSON.stringify(data))
+		}
 
-	ws.onmessage = function (event) {
-	  if (event.data == "yes") {
-		  $("#steamdownload1").attr("style", "")
-		  $("#steamdownload1").attr("value", "Download from SteamCmd")
-		  $("#steamdownload1").attr("class", "btn_medium  btn_border_2px btn_green_white_innerfade")
-	  }
-	  if (event.data == "no") {
-		  $("#steamdownload1").attr("value", "Game is not availible")
-		  $("#steamdownload1").attr("style", "pointer-events:none;")
-	  }
+		ws.onmessage = function (event) {
+		  if (event.data == "yes") {
+			  $("#steamdownload1").attr("style", "")
+			  $("#steamdownload1").attr("value", "Download from SteamCmd")
+			  $("#steamdownload1").attr("class", "btn_medium  btn_border_2px btn_green_white_innerfade")
+		  }
+		  if (event.data == "no") {
+			  $("#steamdownload1").attr("value", "Game is not availible")
+			  $("#steamdownload1").attr("style", "pointer-events:none;")
+		  }
+		}
+		
+		ws.onclose = function () {
+			$("#steamdownload1").attr("style", "pointer-events:none;")
+			$("#steamdownload1").attr("value", "connecting...")
+			$("#steamdownload1").attr("class", "btn_medium  btn_border_2px")
+		  setTimeout(function() {
+					openWebSocket()
+			}, 100)
+		}
+		
+		$("#steamdownload1").click(function()
+		{
+			data.ActionID = 0
+			ws.send(JSON.stringify(data))
+		});
 	}
 	
-	$("#steamdownload1").click(function()
-    {
-		data.ActionID = 0
-		ws.send(JSON.stringify(data))
-    });
+	openWebSocket()
 })
